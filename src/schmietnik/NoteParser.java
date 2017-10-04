bac0ebf2c04f6d22ac77d8726d29727427e861cf
Plasmoxy <sebohacker@gmail.com>
2017-10-04 16:54:53 +0200

    finally works

--- src/schmietnik/NoteParser.java
+++ src/schmietnik/NoteParser.java
@@ -2,11 +2,12 @@ package schmietnik;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
 
-// note string to int[<midiTrack>][<midiNoteValue] array parser by Plasmoxy
+// multitrack note string to int[<midiTrack>][<midiNoteValue] array parser by Plasmoxy
 // Notes available from C0 to G10
 // syntax : <noteLetter><shift><octave>
 // shift -> "-" for no shift, "#" for up, "b" for down
@@ -16,29 +17,12 @@ import javax.swing.text.html.HTMLDocument.Iterator;
 public class NoteParser
 {
 	
-	private static void debugArray(Object[] o)
-	{
-		System.out.println(Arrays.deepToString(o));
-	}
-	
-	private static void debugStrList(List<List<String>> a)
-	{
-		for (List<String> l : a)
-		{
-			for (String s : l)
-			{
-				System.out.print(s);
-				System.out.print("|");
-			}
-			System.out.print(":");
-		}
-		
-	}
-	
-	public static int parseNote(String note) throws NoteParseException
+	// Parses note using the 3 character format
+	private static int parseNote(String note) throws NoteParseException
 	{
 		int midiValue = 0;
 		
+		// follows the format
 		char letter = note.charAt(0);
 		char shift = note.charAt(1);
 		char octave = note.charAt(2);
@@ -56,8 +40,8 @@ public class NoteParser
 		case 'G' : noteNum = 7 ;break;
 		case 'A' : noteNum = 9 ;break;
 		case 'B' : noteNum = 11 ;break;
-		case '-' : return -1 ;
-		case '.' : return -2 ;
+		case '-' : return -1 ; // an empty note, just straight return empty note -1 code
+		case '.' : return -2 ; // extender, return -2 extender code
 		default :
 			throw new NoteParseException();
 		}
@@ -77,9 +61,9 @@ public class NoteParser
 		}
 		
 		
-		midiValue = noteNum + octaveNum*12 + shiftNum;
+		midiValue = noteNum + octaveNum*12 + shiftNum; // thanks to revolutionary midi system, now we can actually have symetrical note logic
 		
-		if ( ( midiValue < 0 || midiValue > 127 )) {
+		if ( ( midiValue < 0 || midiValue > 127 )) { // in case some genius wanted to use more than 128 key piano
 			throw new NoteParseException();
 		}
 		
@@ -88,45 +72,53 @@ public class NoteParser
 	}
 	
 	
+	// Parses the whole text data 
 	public static int[][] niceParse(String str) throws NoteParseException 
 	{	
 		
-		List<String> strTracks;
 		
-		if (str.contains("\n"))
+		List<String> strTracks = new ArrayList<String>(); // initialize temporary arraylist to store track strings
+		List<List<String>> strNotes = new ArrayList<List<String>>(); // the main 2D ArrayList with string notes
+		
+		// --- Here we split the whole data to tracks by newline ( and carriage ) ---
+		if (str.contains("\n")) // see if there's newline
 		{
-			strTracks = Arrays.asList(str.split("\n")); // split to tracks
-		} else {
-			strTracks = new ArrayList<String>(); // in case of only one track
+			System.out.println("matches");
+			strTracks = Arrays.asList(str.split("\\r?\\n")); // AMAZINGLY split to tracks
+		} else { // if not then its only one track
+			strTracks = new ArrayList<String>();
 			strTracks.add(str);
 		}
 		
-		strTracks.remove(null);
-		strTracks.remove("\n");
+		strTracks.removeAll(Collections.singleton("\r")); // get rid of carriage return bitch String if its stuck somewhere
 		
-		List<List<String>> strNotes = new ArrayList<List<String>>();
+		// --- Here we split each track by space so we can have individual notes
 		
 		for (String track : strTracks)
 		{
 			if (track.contains(" "))
 			{
 				strNotes.add(Arrays.asList(track.split(" ")));
-			} else {
-				List<String> currentTrackRef = new ArrayList<String>(); // create object, tempotary reference
-				currentTrackRef.add(track); // add the string to it
-				strNotes.add(currentTrackRef); // add the object
+			} else { // if only one note
+				List<String> singleNoteList = new ArrayList<String>();
+				singleNoteList.add(track);
+				strNotes.add(singleNoteList);
 			}
 		}
 		
-		strNotes.remove(null);
-		for (List<String> l : strNotes)
+		strTracks = null; // dereference this because the inner Lists are reprogrammed to strNotes
+		
+		// -- Here we get rid of some bullshit ---
+		for (List<String> trak : strNotes)
 		{
-			l.remove(" ");
+			trak.removeAll(Collections.singleton("")); // every track String can have empty Strings because it splits by spaces and there can be 2 spaces next to each other
+			trak.removeAll(Collections.singleton("\t")); // also get rid of tabs
 		}
 		
-		debugStrList(strNotes);
 		
-		int[][] notes = new int[strNotes.size()][];
+		// -- And here we parse the strNotes 2D String array to 2D int array notes which is our return type
+		
+		int[][] notes = new int[strNotes.size()][]; // int array with unallocated int arrays inside
 		
 		try {
 			
